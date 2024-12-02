0d0c98514c0079f8e0967094a8dcbc580a782d9e
Linksapp <nikitindima1500@gmail.com>
2024-12-02 22:04:03 +0300

    Init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [{
+        protocol: 'https',
+        hostname: 'eu-images.contentstack.com',
+        port: '',
+        pathname: '/v3/assets/**'
+    }],
+  }
 };
 
 export default nextConfig;
